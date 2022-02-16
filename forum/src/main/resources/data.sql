b763eb0882418abb74eb849330645a27c27d7e92
Emmanuel Casangel <emmanuel.casangel@ifood.com.br>
2022-02-16 00:10:29 -0300

    adicionando autenticação de usuario

--- forum/src/main/resources/data.sql
+++ forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$o9CL4L0CfJ1AStB2ZxVvtOQ0H.1ukSj/8x4Ge0WNPp87ab4PTBTNy');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
