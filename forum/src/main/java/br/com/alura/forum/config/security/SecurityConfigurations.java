a6a4336b66a9d67368e5d31387375e8999ff573b
Emmanuel Casangel <emmanuel.casangel@ifood.com.br>
2022-02-16 00:11:07 -0300

    removing unsed method

--- forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -35,8 +35,4 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
     }
 
-    public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("123456"));
-    }
-
 }
